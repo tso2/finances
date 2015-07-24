0e3c607afc9690404ed7421a1a4bd968b66bddc5
Tyler Sorg <tso2@pdx.edu>
2015-07-24 00:35:51 -0700

    first commit

---
+++ src/main/java/com/jamesshore/finances/SavingsAccountYear.java
@@ -0,0 +1,88 @@
+package com.jamesshore.finances;
+
+public class SavingsAccountYear {
+
+	private int startingPrincipal = 0;
+	private int startingBalance = 0;
+//	private int capitalGainsAmount = 0;
+	private int interestRate = 0;
+	private int totalWithdrawn = 0;
+	
+/*
+	public SavingsAccountYear(int startingBalance, int interestRate) {
+		this.startingBalance = startingBalance;
+		this.interestRate = interestRate;
+	}
+	*/
+
+	public SavingsAccountYear(int startingBalance, int startingPrincipal, int interestRate) {
+		this.startingBalance = startingBalance;
+		this.startingPrincipal = startingPrincipal;
+//		this.capitalGainsAmount = startingBalance - startingPrincipal;
+		this.interestRate = interestRate;
+	}
+
+	public void deposit(int amount) {
+		startingBalance += amount;
+	}
+
+	public void withdraw(int amount) {
+		//startingBalance -= amount;
+		this.totalWithdrawn  += amount;
+		
+	}
+
+	public int totalWithdrawn() {
+		return totalWithdrawn;
+	}
+	
+	public SavingsAccountYear nextYear(int capitalGainsTaxRate) {
+		return new SavingsAccountYear(this.endingBalance(capitalGainsTaxRate), startingPrincipal, interestRate);
+	}
+
+	public int startingBalance() {
+		return startingBalance;
+	}
+	
+	public int startingPrincipal() {
+		//return startingBalance - capitalGainsAmount;
+		return startingPrincipal;
+	}
+
+	public int startingCapitalGains() {
+		return startingBalance - startingPrincipal;
+	}
+
+	public int endingBalance(int capitalGainsTaxRate) {
+		int modifiedStart = startingBalance() - totalWithdrawn() - capitalGainsTaxIncurred(capitalGainsTaxRate);
+		return modifiedStart + (modifiedStart * interestRate / 100);
+	}
+	
+	public int endingPrincipal() {
+		int result = startingPrincipal() - totalWithdrawn();
+		return  Math.max(0, result);
+	}
+	
+	public int interestRate() {
+		return interestRate;
+	}
+
+
+	public int capitalGainsWithdrawn() {
+		int result = -(startingPrincipal() - totalWithdrawn());
+		return  Math.max(0, result);
+	}
+
+	public int capitalGainsTaxIncurred(int taxRate) {
+		double dblTaxRate = taxRate/100.0;
+		double dblCapitalGainsWithdrawn = capitalGainsWithdrawn();
+		//return (int) (capitalGainsWithdrawn() / (1 - dblTaxRate)) - capitalGainsWithdrawn();
+		return (int) (dblCapitalGainsWithdrawn / (1 - dblTaxRate) - dblCapitalGainsWithdrawn);
+	}
+
+
+	public int endingCapitalGains() {
+		// TODO VIDEO 9
+		return 0;
+	}
+}
---
+++ src/test/java/com/jamesshore/finances/_SavingsAccountYearTest.java
@@ -0,0 +1,139 @@
+package com.jamesshore.finances;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class _SavingsAccountYearTest {
+	
+
+	@Test
+	public void startingBalanceMatchesConstructor(){
+		SavingsAccountYear account = newAccount();
+		assertEquals(10000, account.startingBalance());
+	}
+
+	@Test
+	public void interestRateMatchesConstructor() {
+		SavingsAccountYear account = newAccount();
+		assertEquals(10, account.interestRate());
+	}
+
+	@Test
+	public void startingPrincipalMatchesConstructor() {
+		SavingsAccountYear year = new SavingsAccountYear(10000, 3000, 10);
+		assertEquals(3000, year.startingPrincipal());
+	}
+
+	@Test
+	public void startingCapitalGainsIsStartingBalanceMinusStartingPrincipal() {
+		SavingsAccountYear year = new SavingsAccountYear(10000, 3000, 10);
+		assertEquals(7000, year.startingCapitalGains());
+	}
+
+	@Test
+	public void endingPrincipalConsidersWithdrawals() {
+		SavingsAccountYear year = new SavingsAccountYear(10000, 3000, 10);
+		year.withdraw(2000);
+		assertEquals("ending principal", 1000, year.endingPrincipal());
+	}
+
+	@Test
+	public void endingPrincipalNeverGoesBelowZero() {
+		SavingsAccountYear year = new SavingsAccountYear(10000, 3000, 10);
+		year.withdraw(4000);
+		assertEquals("ending principal", 0, year.endingPrincipal());
+	}
+
+	@Test
+	public void endingCapitalGainsIncludesInterestEarned() {
+		SavingsAccountYear year = new SavingsAccountYear(10000, 3000, 10);
+		assertEquals(7000, year.startingCapitalGains());
+		assertEquals(4000, year.endingCapitalGains());
+	}
+
+	@Test
+	public void endingBalanceAppliesInterestRate() {
+		SavingsAccountYear account = newAccount();
+		assertEquals(11000, account.endingBalance(25));
+
+	}
+
+	@Test
+	public void withdrawnFundsDoNotEarnInterest() {
+		SavingsAccountYear year = newAccount();
+		year.withdraw(1000);
+		assertEquals(9900, year.endingBalance(25));
+	}
+
+	@Test
+	public void multipleWithdrawalsInAYear() {
+		SavingsAccountYear year = newAccount();
+		year.withdraw(1000);
+		year.withdraw(2000);
+		assertEquals(3000, year.totalWithdrawn());
+		
+	}
+
+	@Test
+	public void withdrawingMoreThanPrincipalTakesFromCapitalGains() {
+		SavingsAccountYear year = new SavingsAccountYear(10000, 3000, 10);
+		year.withdraw(1000);
+		assertEquals(0, year.capitalGainsWithdrawn());
+		year.withdraw(3000);
+		assertEquals("Withdrew 1000 more than principal balance", 1000, year.capitalGainsWithdrawn());
+	}
+
+	@Test
+	public void capitalGainsTaxIncurred_needsToCoverCapitalGainsWithdrawn_AND_theAdditionalCapitalGainsWithdrawnToPayTheCapitalGainsTax() {
+		/*
+		Example of concept:
+		1000 withdrawn
+		10% tax rate
+      1000 * 0.10 = 100
+      100  * 0.10 = 10
+      10   * 0.10 = 1
+      ...
+      100 + 10 + 1 extra is needed to withdraw and pay tax
+      ... Using this formula:
+      1000/(inverse of rate) - 1000 = 111
+        1000/(1 - 0.10) - 1000 = 111 (actually 111.1111...)
+		 */
+		SavingsAccountYear year = new SavingsAccountYear(10000, 3000, 10);
+		year.withdraw(5000);
+		assertEquals(2000, year.capitalGainsWithdrawn());
+		//2000/(1-0.25) - 2000 = 666.66...
+		assertEquals(666, year.capitalGainsTaxIncurred(25));
+	}
+
+	@Test
+	public void nextYearsStartingBalanceShouldEqualThisYearsEndingBalance() {
+		SavingsAccountYear account = newAccount();
+		assertEquals(account.endingBalance(25), account.nextYear(25).startingBalance());
+	}
+
+	@Test
+	public void nextYearsInterestRateEqualsThisYearsInterestRate() {
+		SavingsAccountYear thisYear = newAccount();
+		assertEquals(thisYear.interestRate(), thisYear.nextYear(25).interestRate());
+	}
+
+	@Test
+	public void capitalGainsTaxIsIncludedInEndingBalance() {
+		SavingsAccountYear year = new SavingsAccountYear(10000, 3000, 10);
+		int amountWithdrawn = 5000;
+		year.withdraw(amountWithdrawn);
+		int expectedCapitalGainsTax = 666;
+		assertEquals(expectedCapitalGainsTax, year.capitalGainsTaxIncurred(25));
+		int expectedBalanceAfterWithdrawals = 10000 - amountWithdrawn - expectedCapitalGainsTax;
+		assertEquals((int)(expectedBalanceAfterWithdrawals * 1.10), year.endingBalance(25));
+	}
+
+	private SavingsAccountYear newAccount() {
+		SavingsAccountYear account = new SavingsAccountYear(10000, 10000,10);
+		return account;
+	}
+
+}
